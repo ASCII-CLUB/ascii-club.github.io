e09a13ff188e7bdb4fe2843b741009249b1a2538
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-08-18 16:48:52 -0400

    Make the site look cool

---
+++ script.js
@@ -0,0 +1,32 @@
+async function submitContactForm() {
+	const name = document.getElementById("contact-name").value;
+	const email = document.getElementById("contact-email").value;
+	const comment = document.getElementById("contact-comment").value;
+
+	const webhookbody = {
+		embeds: [{
+			title: "Contact Form (website)",
+			fields: [
+				{ name: "name", value: name },
+				{ name: "email", value: email },
+				{ name: "comment", value: comment },
+			]
+		}]
+	};
+
+	const webhookurl = "https://discord.com/api/webhooks/1407097018718879925/LF_CT2JOeN_O2qxuBJVmQogeEcSPzIKYfxBjYw_g0Aii-MPO4mi9A1TC1mayNjilw51S";
+
+	const response = await fetch(webhookurl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(webhookbody),
+	});
+
+	if (response.ok) {
+		alert("Message has been recieved by club officers");
+	} else {
+		alert("There was an error, please try again later");
+	}
+}
