e64fd50f010adbdb7518fc261265762c2099649b
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-08-18 23:30:29 -0400

    Added container, and added dev/prod webhook urls

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// ENV Vars
+const prodURL = "ascii-club.github.io"
+const baseURL = window.location.hostname;
+const isProd = prodURL == baseURL;
+const devDiscordContactWebhook = "https://discord.com/api/webhooks/1407095671072358513/w3mHXxzAfBaj45iyQMkKHgBCWtZgQITtXqL2_4DW_HsCOa-eRIMLq938svmoBcunu1Kz";
+const prodDiscordContactWebhook = "https://discord.com/api/webhooks/1407097018718879925/LF_CT2JOeN_O2qxuBJVmQogeEcSPzIKYfxBjYw_g0Aii-MPO4mi9A1TC1mayNjilw51S";
+
 async function submitContactForm(ev) {
 	ev.preventDefault();
 	const name = document.getElementById("contact-name").value;
@@ -15,7 +22,7 @@ async function submitContactForm(ev) {
 		}]
 	};
 
-	const webhookurl = "https://discord.com/api/webhooks/1407095671072358513/w3mHXxzAfBaj45iyQMkKHgBCWtZgQITtXqL2_4DW_HsCOa-eRIMLq938svmoBcunu1Kz";
+	const webhookurl = isProd ? prodDiscordContactWebhook : devDiscordContactWebhook;
 
 	const response = await fetch(webhookurl, {
 		method: "POST",
@@ -26,6 +33,7 @@ async function submitContactForm(ev) {
 	});
 
 	if (response.ok) {
+		document.getElementById("contact-submit").style.display = "none";
 		alert("Message has been recieved by club officers");
 	} else {
 		alert("There was an error, please try again later");
@@ -34,4 +42,8 @@ async function submitContactForm(ev) {
 
 function horse() {
 	window.location.href = "/images/images.jpg";
+}
+
+function kat() {
+	window.location.href = "https://kittkat.xyz/";
 }
